102665cfc99bcbfd0e779fff51b7c151b1921321
Estefania Morton <estefania.morton@gmail.com>
2016-12-06 17:41:14 -1000

    finished

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
-};
+
+      var number = Math.random();
+
+      function randomNumber() {
+        Math.random()
+        return number;
+
+      }
+
+      function secretNumber() {
+        //number = randomNumber();
+        return number;
+      }
+
+      return secretNumber
+
+}
